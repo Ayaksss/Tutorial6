075db31285727b2082966fea28b8ee6579eaf7c4
Ayaksss <s32770@pjwstk.edu.pl>
2026-04-20 20:38:46 +0200

    added static do class

--- Tutorial6/Helpers/DataStore.cs
+++ Tutorial6/Helpers/DataStore.cs
@@ -2,7 +2,7 @@
 using Tutorial6.Models;
 using Tutorial6.Helpers;
 
-public class DataStore
+public static class DataStore
 { 
         public static List<Room> Rooms = CreateList.GetRooms(); 
         public static List<Reservation> Reservations = CreateList.GetReservations();
